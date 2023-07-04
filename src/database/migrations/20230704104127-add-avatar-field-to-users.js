8da5dac3a0b4c6ff8f0457afa2e4197b07a87c14
caioadauri <caioadaurigomes@gmail.com>
2023-07-04 08:01:12 -0300

    adicionando avatar no banco

---
+++ src/database/migrations/20230704104127-add-avatar-field-to-users.js
@@ -0,0 +1,16 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface) {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
