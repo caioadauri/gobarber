0af3f9237281a6bff1b50db1c986349a71d5ec28
caioadauri <caioadaurigomes@gmail.com>
2023-07-06 19:24:08 -0300

    adiciona conexao mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
 
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
@@ -11,6 +12,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -21,6 +23,12 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models)
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber'
+    );
   }
 }
 
