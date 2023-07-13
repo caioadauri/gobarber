252bcad56f49bac438135b9a1413d6471d154dc7
caioadauri <caioadaurigomes@gmail.com>
2023-07-13 12:52:03 -0300

    configurando o envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'sandbox.smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '81d8b7844697d0',
+    pass: '216b2d3dead3ea',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@haritov.com.br>',
+  },
+};
